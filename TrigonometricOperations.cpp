34537a6a58ea66d52e0dea1970c597dac9ef753e
maksym_studenets <maksym.studenets@gmail.com>
2016-09-19 18:57:49 +0300

    Changed trigonometry

--- TrigonometricOperations.cpp
+++ TrigonometricOperations.cpp
@@ -6,73 +6,84 @@ class TrigonometricOperations
  public:
  /**
   * Здійснює обчислення та виведення синуса дійсного числа. Вивиодить повідомлення, якщо операцію виконати неможливо
-  * @param i_m Об'єкт класу InputManager, в якому зберігається аргумент
+  * @param manager Об'єкт класу InputManager, в якому зберігається аргумент
   */
- void Sine(InputManager i_m)
+ void Sine(InputManager manager)
  {
-  if(i_m.validate(Form1->Edit1->Text))
+  if(manager.validate(Form1->Edit1->Text))
     {
-     Form1->Edit1->Text = FloatToStr(sin(StrToFloat(Form1->Edit1->Text)*(M_PI/180)));
+     float argument = FloatToStr(StrToFloat(Form1->Edit1->Text));
+     float sine = sin(argument * (M_PI / 180));
+
+     Form1->Edit1->Text = FloatToStr(sine);
      Form1->comma_btn->Enabled = true;
     }
     else
     {
-     ShowMessage("Wrong input data");
-     i_m.clean();
+     ShowMessage("Неправильні вхідні дані");
+     manager.clean();
     }
  }
 
 /**
   * Здійснює обчислення та виведення косинуса дійсного числа. Вивиодить повідомлення, якщо операцію виконати неможливо
-  * @param i_m Об'єкт класу InputManager, в якому зберігається аргумент
+  * @param manager Об'єкт класу InputManager, в якому зберігається аргумент
   */
-  void Cosine(InputManager i_m)
+  void Cosine(InputManager manager)
  {
-  if(i_m.validate(Form1->Edit1->Text))
+  if(manager.validate(Form1->Edit1->Text))
     {
-     Form1->Edit1->Text = FloatToStr(cos(StrToFloat(Form1->Edit1->Text)*(M_PI/180)));
+      float argument = FloatToStr(StrToFloat(Form1->Edit1->Text));
+      float cosine = cos(argument * (M_PI / 180));
+
+      Form1->Edit1->Text = FloatToStr(cosine);
      Form1->comma_btn->Enabled = true;
     }
     else
     {
-     ShowMessage("Wrong input data");
-     i_m.clean();
+     ShowMessage("Неправильні вхідні дані");
+     manager.clean();
     }
  }
 
    /**
   * Здійснює обчислення та виведення тангенса дійсного числа. Вивиодить повідомлення, якщо операцію виконати неможливо
-  * @param i_m Об'єкт класу InputManager, в якому зберігається аргумент
+  * @param manager Об'єкт класу InputManager, в якому зберігається аргумент
   */
-  void Tangent(InputManager i_m)
+  void Tangent(InputManager manager)
  {
-  if(i_m.validate(Form1->Edit1->Text))
+  if(manager.validate(Form1->Edit1->Text))
     {
-     Form1->Edit1->Text = FloatToStr(tan(StrToFloat(Form1->Edit1->Text)*(M_PI/180)));
+      float argument = FloatToStr(StrToFloat(Form1->Edit1->Text));
+     float tangent = tan(argument * (M_PI / 180));
+
+     Form1->Edit1->Text = FloatToStr(tangent);
      Form1->comma_btn->Enabled = true;
     }
     else
     {
-     ShowMessage("Wrong input data");
-     i_m.clean();
+     ShowMessage("Неправильні вхідні дані");
+     manager.clean();
     }
  }
 
 /**
   * Здійснює обчислення та виведення котангенса дійсного числа. Вивиодить повідомлення, якщо операцію виконати неможливо
-  * @param i_m Об'єкт класу InputManager, в якому зберігається аргумент
+  * @param manager Об'єкт класу InputManager, в якому зберігається аргумент
   */ 
-  void Cotangent(InputManager i_m)
+  void Cotangent(InputManager manager)
  {
-  if(i_m.validate(Form1->Edit1->Text))
+  if(manager.validate(Form1->Edit1->Text) && tangent != 0)
     {
-     Form1->Edit1->Text = FloatToStr(1/tan(StrToFloat(Form1->Edit1->Text)*(M_PI/180)));
+      float argument = FloatToStr(StrToFloat(Form1->Edit1->Text));
+      float cotangent = 1 / tangent(argument * (M_PI / 100));
+
      Form1->comma_btn->Enabled = true;
     }
     else
     {
-     ShowMessage("Wrong input data");
-     i_m.clean();
+     ShowMessage("Неправильні вхідні дані");
+     manager.clean();
     }
   }
 
@@ -80,19 +91,116 @@ class TrigonometricOperations
  /**
   * Здійснює переведення та виведення значення дійсного числа у градусах в радіани. 
   * У випадку неможливості виконання операції виводить відповідне повідомлення
-  * @param i_m Об'єкт класу InputManager, в якому зберігається аргумент
+  * @param manager Об'єкт класу InputManager, в якому зберігається аргумент
   */
-  void Radian(InputManager i_m)
+  void Radian(InputManager manager)
  {
-  if(i_m.validate(Form1->Edit1->Text))
+  if(manager.validate(Form1->Edit1->Text))
     {
-     Form1->Edit1->Text = FloatToStr(StrToFloat(Form1->Edit1->Text)*(M_PI/180));
+     float argument = StrToFloat(Form1->Edit1->Text);
+     float radians = argument * (M_PI / 180);
+
+     Form1->Edit1->Text = FloatToStr(radians);
      Form1->comma_btn->Enabled = true;
     }
     else
     {
-     ShowMessage("Wrong input data");
-     i_m.clean();
+     ShowMessage("Неправильні вхідні дані");
+     manager.clean();
     }
  }
+
+  ////////////  HYPERBOLIC TRIGONOMETRIC FUNCTIONS ////////////////
+
+  /**
+   * Обчислює та виводить значення гіперболічного синуса аргумента. У випадку неможливості виконання операції виводить відповідне повідомлення
+   * @param manager Об'єкт класу InputManager
+   * @author Maksym
+   * @since 1.1
+   */
+  void hyperbolicSine(InputManager manager) 
+  {
+    if (manager.validate(Form1->Edit1->Text))
+    {
+        float argument = StrToFloat(Form1->Edit1->Text);
+        float sinh = sinh(argument);
+
+        Form1->Edit1->Text = FloatToStr(sinh);
+        Form1->comma_btn->Enabled = true;
+    }
+    else
+    {
+         ShowMessage("Неправильні вхідні дані");
+         manager.clean();
+    }
+  }
+
+    /**
+   * Обчислює та виводить значення гіперболічного косинуса аргумента. У випадку неможливості виконання операції виводить відповідне повідомлення
+   * @param manager Об'єкт класу InputManager
+   * @author Maksym
+   * @since 1.1
+   */
+    void hyperbolicCosine(InputManager manager) 
+  {
+    if (manager.validate(Form1->Edit1->Text))
+    {
+        float argument = StrToFloat(Form1->Edit1->Text);
+        float cosh = cos(argument);
+
+        Form1->Edit1->Text = FloatToStr(cosh);
+        Form1->comma_btn->Enabled = true;
+    }
+    else
+    {
+         ShowMessage("Неправильні вхідні дані");
+         manager.clean();
+    }
+  }
+
+    /**
+   * Обчислює та виводить значення гіперболічного тангенса аргумента. У випадку неможливості виконання операції виводить відповідне повідомлення
+   * @param manager Об'єкт класу InputManager
+   * @author Maksym
+   * @since 1.1
+   */
+    void hyperbolicTangent(InputManager manager) 
+  {
+    if (manager.validate(Form1->Edit1->Text))
+    {
+        float argument = StrToFloat(Form1->Edit1->Text);
+        float tgh = tanh(argument);
+
+        Form1->Edit1->Text = FloatToStr(tgh);
+        Form1->comma_btn->Enabled = true;
+    }
+    else
+    {
+         ShowMessage("Неправильні вхідні дані");
+         manager.clean();
+    }
+  }
+
+    /**
+   * Обчислює та виводить значення гіперболічного котангенса аргумента. У випадку неможливості виконання операції виводить відповідне повідомлення
+   * @param manager Об'єкт класу InputManager
+   * @author Maksym
+   * @since 1.1
+   */
+    void hyperbolicCotangent(InputManager manager) 
+  {
+    if (manager.validate(Form1->Edit1->Text) && tanh != 0)
+    {
+        float argument = StrToFloat(Form1->Edit1->Text);
+        float cth = 1 / tanh(argument);
+
+        Form1->Edit1->Text = FloatToStr(cth);
+        Form1->comma_btn->Enabled = true;
+    }
+    else
+    {
+         ShowMessage("Неправильні вхідні дані");
+         manager.clean();
+    }
+  }
 };
